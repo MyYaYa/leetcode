d9115ca9627c479df8a22b36e948702a80207722
薄一丁 <boyiding1@gmail.com>
2018-04-02 09:18:48 +0800

    Update: Offer Problem046

--- offer/problem046/Solution2.cpp
+++ offer/problem046/Solution2.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int GetMissingNumber(vector<int> data) {
+    int GetNumberSameAsIndex(vector<int> data) {
         if (data.empty())   return -1;
         int left = 0, right = data.size() - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
             if (data[mid] == mid) {
-                left = mid + 1;
-            } else if ((mid > 0 && data[mid - 1] == mid - 1) || mid == 0) {
                 return mid;
-            } else {
+            } else if (data[mid] > mid) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return left;
+        return -1;
     }
 };
